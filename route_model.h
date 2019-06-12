69c7ecb0cf405a756b3a30f1c21ee3f080a55f62
Aati Singh <atindrasingh1511@gmail.com>
2019-06-12 13:36:17 -0500

    Update route_model.h

--- route_model.h
+++ route_model.h
@@ -21,7 +21,7 @@ class RouteModel : public Model {
       std::vector<Node *> neighbors;
       void FindNeighbors();
       float distance(Node other) const {
-        return std::sqrt(std::pow(2,(x- other.x)) +std::pow(2,(y- other.y)));
+        return std::sqrt(std::pow((x- other.x),2) +std::pow((y- other.y),2));
       }
         
         Node(){}
